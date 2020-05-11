a7638930340759284dd78080ecea2793902fc2bb
tom_glb <goodMorning_glb@atguigu.com>
2020-05-12 01:04:27 +0800

    Add qiNiuYun

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
 import com.example.demo.http.TestServer;
+import com.example.demo.qiniu.QiNiuServiceImpl;
 import com.example.demo.simple.NettyServer;
+import com.qiniu.common.QiniuException;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.Resource;
+import java.io.File;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -15,7 +18,9 @@ public class DemoApplication implements CommandLineRunner {
     private NettyServer nettyServer;
     @Resource
     private TestServer testServer;
-    public static void main(String[] args) {
+    @Resource
+    protected   QiNiuServiceImpl qiNiuService;
+    public static void main(String[] args) throws QiniuException {
         SpringApplication.run(DemoApplication.class, args);
     }
 
@@ -26,7 +31,10 @@ public class DemoApplication implements CommandLineRunner {
      */
     @Override
     public void run(String... args) throws Exception {
-        testServer.start();
-        nettyServer.start();
+//        D:\javaProject\controlMeServices\tmp\1.png
+        File target = new File("D:\\javaProject\\controlMeServices\\tmp\\1.png");
+        qiNiuService.uploadFile(target);
+//        testServer.start();
+//        nettyServer.start();
     }
 }
